206398728b4f4d04f94e2b876548da6b1c7a2d20
Bruce Berrios <bberr022@fiu.edu>
2020-09-21 14:16:34 -0400

    Test queue

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "process.h"
+#include "queue.h"
 
 int main() {
-  printf("Hello World!\n");
+  queue* test_queue = create_queue();
+  process* test_process = create_process(0, 10, 20);
+  process* test_process2 = create_process(0, 20, 30);
+  print_queue(test_queue);
+  add_to_queue(test_queue, test_process);
+  add_to_queue(test_queue, test_process2);
+  print_queue(test_queue);
+  remove_from_queue(test_queue);
+  print_queue(test_queue);
+  remove_from_queue(test_queue);
+  print_queue(test_queue);
+  add_to_queue(test_queue, test_process2);
+  add_to_queue(test_queue, test_process);
+  print_queue(test_queue);
   return 0;
 }
